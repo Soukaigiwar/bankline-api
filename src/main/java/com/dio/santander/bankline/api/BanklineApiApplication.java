7d7905d1bb6d8e0f2d2cd838528ff4372ff2ae05
Sergio Mello <54086765+Soukaigiwar@users.noreply.github.com>
2022-05-04 00:16:19 -0300

    restarted project

--- src/main/java/com/dio/santander/bankline/api/BanklineApiApplication.java
+++ src/main/java/com/dio/santander/bankline/api/BanklineApiApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BanklineApiApplication {
 
-//	aqui começa o app
 	public static void main(String[] args) {
 		SpringApplication.run(BanklineApiApplication.class, args);
 	}
