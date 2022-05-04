7c38e98a4c2a375dab05b833793efc81abf9f402
Sergio Mello <54086765+Soukaigiwar@users.noreply.github.com>
2022-05-03 22:56:13 -0300

    autosave enabled

--- src/main/java/com/dio/santander/bankline/api/BanklineApiApplication.java
+++ src/main/java/com/dio/santander/bankline/api/BanklineApiApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BanklineApiApplication {
 
+//	aqui começa o app
 	public static void main(String[] args) {
 		SpringApplication.run(BanklineApiApplication.class, args);
 	}
