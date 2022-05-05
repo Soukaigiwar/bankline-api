82c91677f0751b75b2e69d9d40bfe54b291bf55e
Sergio Mello <54086765+Soukaigiwar@users.noreply.github.com>
2022-05-05 17:39:43 -0300

    Revert "Update MovimentacaoService.java"
    This reverts commit 03a3ef3cb329144268bdc5c641e3c86794804099.

--- src/main/java/com/dio/santander/bankline/api/service/MovimentacaoService.java
+++ src/main/java/com/dio/santander/bankline/api/service/MovimentacaoService.java
@@ -6,19 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.dio.santander.bankline.api.dto.NovaMovimentacao;
-import com.dio.santander.bankline.api.model.Correntista;
 import com.dio.santander.bankline.api.model.Movimentacao;
 import com.dio.santander.bankline.api.model.MovimentacaoTipo;
-import com.dio.santander.bankline.api.repository.CorrentistaRepository;
 import com.dio.santander.bankline.api.repository.MovimentacaoRepository;
 
 @Service
 public class MovimentacaoService {
 	@Autowired
 	private MovimentacaoRepository repository;
-	
-	@Autowired
-	private CorrentistaRepository correntistaRepository;
 	public void save(NovaMovimentacao novaMovimentacao) {
 		Movimentacao movimentacao = new Movimentacao();
 		Double valor = novaMovimentacao.getTipo()==MovimentacaoTipo.RECEITA ? novaMovimentacao.getValor() : novaMovimentacao.getValor() * -1;
@@ -28,11 +23,6 @@ public class MovimentacaoService {
 		movimentacao.setTipo(novaMovimentacao.getTipo());
 		movimentacao.setValor(valor);
 		
-		Correntista correntista = correntistaRepository.findById(novaMovimentacao.getIdConta()).orElse(null);
-		if(correntista != null) {
-			correntista.getConta().setSaldo(correntista.getConta().getSaldo() + valor);
-			correntistaRepository.save(correntista);
-		}
 		repository.save(movimentacao);
 	}
 }
