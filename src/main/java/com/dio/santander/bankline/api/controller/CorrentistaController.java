e015f4539b6e2b134825c915dcb5fb45588de828
Sergio Mello <54086765+Soukaigiwar@users.noreply.github.com>
2022-05-05 00:56:07 -0300

    request correntista service

--- src/main/java/com/dio/santander/bankline/api/controller/CorrentistaController.java
+++ src/main/java/com/dio/santander/bankline/api/controller/CorrentistaController.java
@@ -3,10 +3,15 @@ package com.dio.santander.bankline.api.controller;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.dio.santander.bankline.api.dto.NovoCorrentista;
 import com.dio.santander.bankline.api.model.Correntista;
 import com.dio.santander.bankline.api.repository.CorrentistaRepository;
+import com.dio.santander.bankline.api.service.CorrentistaService;
 
 @RestController
 @RequestMapping("/correntistas")
@@ -14,8 +19,16 @@ public class CorrentistaController {
 	@Autowired
 	private CorrentistaRepository repository;
 	
+	@Autowired
+	private CorrentistaService service;
+	
 	@GetMapping
 	public List<Correntista> findAll(){
 		return repository.findAll();
+	}
+	
+	@PostMapping
+	public void save(@RequestBody NovoCorrentista correntista) {
+		service.save(correntista);
 	}
 }
